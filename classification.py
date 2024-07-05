38491641ba44533453b4ae89f4ed43fd49f0a392
NON906 <mumeigames@gmail.com>
2024-07-05 13:39:31 +0900

    初期値を指定するように変更

--- classification.py
+++ classification.py
@@ -8,20 +8,24 @@ from PIL import Image
 import numpy as np
 import glob
 
-def main():
-    img_model = SentenceTransformer('clip-ViT-B-32')
-
-    filenames = glob.glob('src_images/**/*.*', recursive=True)
+def calc_embedding(filenames, img_model):
     embedding_array = []
     for filename in tqdm(filenames, total=len(filenames)):
         image = Image.open(filename)
         embedding = img_model.encode(image)
         embedding_array.append(embedding)
     input_embedding = np.stack(embedding_array, 0)
+    return normalize(input_embedding)
 
-    input_embedding = normalize(input_embedding)
+def main():
+    img_model = SentenceTransformer('clip-ViT-B-32')
 
-    kmeans_model = KMeans(n_clusters=2)
+    filenames = glob.glob('src_images/**/*.*', recursive=True)
+    input_embedding = calc_embedding(filenames, img_model)
+    init_filenames = glob.glob('key_images/**/*.*', recursive=True)
+    init_embedding = calc_embedding(init_filenames, img_model)
+
+    kmeans_model = KMeans(n_clusters=2, init=init_embedding)
     kmeans_model.fit(input_embedding)
 
     for loop_cnt, filename in enumerate(filenames):
