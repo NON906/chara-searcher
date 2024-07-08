bf17a0cc8192c13c400ba4507a65b578821845de
NON906 <mumeigames@gmail.com>
2024-07-08 20:04:21 +0900

    standalone用のinstall処理を追加

--- install.py
+++ install.py
@@ -2,6 +2,8 @@
 
 import os
 import shutil
+import subprocess
+import sys
 
 try:
     import launch
@@ -44,6 +46,32 @@ if platform == 'sd-webui':
 
     if not launch.is_installed('chara-searcher requirements'):
         launch.run_pip('install -r requirements.txt', 'chara-searcher requirements')
+
+elif platform == 'standalone':
+    os.chdir(os.path.dirname(__file__))
+    subprocess.run(['git', 'submodule', 'update', '--init', '--recursive'])
+
+    subprocess.run([sys.executable, '-m', 'pip', 'install', '-U', 'openmim'])
+    subprocess.run([sys.executable, '-m', 'mim', 'install', 'mmengine'])
+    subprocess.run([sys.executable, '-m', 'mim', 'install', 'mmcv>=2.0.0'])
+    subprocess.run([sys.executable, '-m', 'mim', 'install', 'mmdet'])
+
+    pip_list_str = subprocess.run([sys.executable, '-m', 'pip', 'list'], capture_output=True, text=True).stdout
+    pip_list_lines = pip_list_str.splitlines()
+    torch_lines = [item for item in pip_list_lines if item.startswith('torch')]
+    torch_version = None
+    if torch_lines and len(torch_lines) > 0:
+        torch_version = torch_lines[0].split()[-1]
+    if torch_version is not None and '+cu' in torch_version:
+        cuda_version = torch_version.split('+cu')[-1]
+        if cuda_version[:2] == '12':
+            subprocess.run([sys.executable, '-m', 'pip', 'install', 'onnxruntime-gpu==1.17.1', '--extra-index-url', 'https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/onnxruntime-cuda-12/pypi/simple/'])
+        else:
+            subprocess.run([sys.executable, '-m', 'pip', 'install', 'onnxruntime-gpu==1.17.1'])
+    else:
+        subprocess.run([sys.executable, '-m', 'pip', 'install', 'onnxruntime==1.17.3'])
+
+    subprocess.run([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
 
 os.chdir(os.path.join(os.path.dirname(__file__), 'CartoonSegmentation'))
 if not os.path.isfile('models/AnimeInstanceSegmentation/rtmdetl_e60.ckpt'):
