ca2082b913105854f6f2393876bdd8d16e0f125c
NON906 <mumeigames@gmail.com>
2025-04-13 12:11:26 +0900

    stable-diffusion-webui-wd14-taggerの併用時のバグ修正

--- tagging.py
+++ tagging.py
@@ -9,15 +9,91 @@ from tqdm import tqdm
 import re
 import cv2
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'stable-diffusion-webui-wd14-tagger'))
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'tagger'))
-sys.path.append(os.path.join(os.path.dirname(__file__), 'wd14-tagger-standalone'))
-try:
-    from tagger.interrogator import Interrogator
-except:
-    pass
-from tagger.interrogators import interrogators
-sys.path = sys.path[:-3]
+tagger_path = None
+if os.path.exists(os.path.join(os.path.dirname(__file__), '..', 'stable-diffusion-webui-wd14-tagger')):
+    tagger_path = os.path.join(os.path.dirname(__file__), '..', 'stable-diffusion-webui-wd14-tagger')
+elif os.path.exists(os.path.join(os.path.dirname(__file__), '..', 'tagger')):
+    tagger_path = os.path.join(os.path.dirname(__file__), '..', 'tagger')
+if tagger_path is not None:
+    sys.path.append(tagger_path)
+    from tagger.interrogator import Interrogator, WaifuDiffusionInterrogator, MLDanbooruInterrogator
+    interrogators: dict[str, Interrogator] = {
+        'wd14-vit.v1': WaifuDiffusionInterrogator(
+            'WD14 ViT v1',
+            repo_id='SmilingWolf/wd-v1-4-vit-tagger'
+        ),
+        'wd14-vit.v2': WaifuDiffusionInterrogator(
+            'WD14 ViT v2',
+            repo_id='SmilingWolf/wd-v1-4-vit-tagger-v2',
+        ),
+        'wd14-convnext.v1': WaifuDiffusionInterrogator(
+            'WD14 ConvNeXT v1',
+            repo_id='SmilingWolf/wd-v1-4-convnext-tagger'
+        ),
+        'wd14-convnext.v2': WaifuDiffusionInterrogator(
+            'WD14 ConvNeXT v2',
+            repo_id='SmilingWolf/wd-v1-4-convnext-tagger-v2',
+        ),
+        'wd14-convnextv2.v1': WaifuDiffusionInterrogator(
+            'WD14 ConvNeXTV2 v1',
+            # the name is misleading, but it's v1
+            repo_id='SmilingWolf/wd-v1-4-convnextv2-tagger-v2',
+        ),
+        'wd14-swinv2-v1': WaifuDiffusionInterrogator(
+            'WD14 SwinV2 v1',
+            # again misleading name
+            repo_id='SmilingWolf/wd-v1-4-swinv2-tagger-v2',
+        ),
+        'wd-v1-4-moat-tagger.v2': WaifuDiffusionInterrogator(
+            'WD14 moat tagger v2',
+            repo_id='SmilingWolf/wd-v1-4-moat-tagger-v2'
+        ),
+        'wd-v1-4-vit-tagger.v3': WaifuDiffusionInterrogator(
+            'WD14 ViT v3',
+            repo_id='SmilingWolf/wd-vit-tagger-v3'
+        ),
+        'wd-v1-4-convnext-tagger.v3': WaifuDiffusionInterrogator(
+            'WD14 ConvNext v3',
+            repo_id='SmilingWolf/wd-convnext-tagger-v3'
+        ),
+        'wd-v1-4-swinv2-tagger.v3': WaifuDiffusionInterrogator(
+            'WD14 SwinV2 v3',
+            repo_id='SmilingWolf/wd-swinv2-tagger-v3'
+        ),
+        'wd-vit-large-tagger-v3': WaifuDiffusionInterrogator(
+            'WD ViT-Large Tagger v3',
+            repo_id='SmilingWolf/wd-vit-large-tagger-v3'
+        ),
+        'wd-eva02-large-tagger-v3': WaifuDiffusionInterrogator(
+            'WD EVA02-Large Tagger v3',
+            repo_id='SmilingWolf/wd-eva02-large-tagger-v3'
+        ),
+        'z3d-e621-convnext-toynya': WaifuDiffusionInterrogator(
+            'Z3D-E621-Convnext(toynya)',
+            repo_id='toynya/Z3D-E621-Convnext',
+            tags_path='tags-selected.csv'
+        ),
+        'z3d-e621-convnext-silveroxides': WaifuDiffusionInterrogator(
+            'Z3D-E621-Convnext(silveroxides)',
+            repo_id='silveroxides/Z3D-E621-Convnext',
+            tags_path='tags-selected.csv'
+        ),
+        'mld-caformer.dec-5-97527': MLDanbooruInterrogator(
+            'ML-Danbooru Caformer dec-5-97527',
+            repo_id='deepghs/ml-danbooru-onnx',
+            model_path='ml_caformer_m36_dec-5-97527.onnx'
+        ),
+        'mld-tresnetd.6-30000': MLDanbooruInterrogator(
+            'ML-Danbooru TResNet-D 6-30000',
+            repo_id='deepghs/ml-danbooru-onnx',
+            model_path='TResnet-D-FLq_ema_6-30000.onnx'
+        ),
+    }
+    sys.path = sys.path[:-1]
+else:
+    sys.path.append(os.path.join(os.path.dirname(__file__), 'wd14-tagger-standalone'))
+    from tagger.interrogators import interrogators
+    sys.path = sys.path[:-1]
 
 from calc_embedding import convert_rgba_to_rgb
 
